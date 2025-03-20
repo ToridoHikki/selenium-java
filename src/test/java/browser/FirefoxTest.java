e437d51f2269e057bd4477cb6731225116404148
Torido Hikigaya <torido@192.168.1.4>
2025-03-20 10:10:59 +0700

    open browser

---
+++ src/test/java/browser/FirefoxTest.java
@@ -0,0 +1,18 @@
+package browser;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class FirefoxTest {
+
+    @Test
+    void openBrowserWithDefaultMode(){
+        WebDriver driver = new FirefoxDriver();
+        driver.get("https://www.selenium.dev/");
+        Assert.assertEquals(driver.getTitle(),"Selenium");
+        driver.quit();
+    }
+
+}
