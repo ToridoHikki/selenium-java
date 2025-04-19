55fd4cc6d9f32e44fe2849d71c938c8fb983bd24
Nhu <truong.thi.huynh.nhu@quantic.com.vn>
2025-04-19 13:29:09 +0700

    add day-6 table check

---
+++ src/test/java/heroku/TableTest.java
@@ -0,0 +1,95 @@
+package heroku;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.OptionalDouble;
+
+public class TableTest {
+
+    @Test
+    void verifyMaxDuePerson () throws InterruptedException {
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/tables");
+
+        List<Person> people = driver
+                .findElements(By.xpath("//table[@id='table1']/tbody/tr"))
+                .stream()
+                .map(WebElement::getText)
+                .map(rowContent -> rowContent.split(" "))
+                .map(cellContent -> new Person(cellContent[0], cellContent[1], cellContent[3]))
+                .toList();
+
+
+        OptionalDouble maxDue = people
+                .stream()
+                .mapToDouble(Person::getDue)
+                .max();
+        List<Person> maxDuePersons = people
+                .stream()
+                .filter(p -> p.getDue() == maxDue.getAsDouble())
+                .toList();
+
+        List<String> maxDuePersonsFullName = maxDuePersons
+                .stream()
+                .map(Person::getFullName)
+                .toList();
+
+        System.out.println("maxDuePersonsFullName = " + maxDuePersonsFullName);
+
+        Assert.assertEquals(maxDuePersonsFullName, List.of("Jason Doe"));
+
+
+
+//        Assert.assertEquals(maxDuePersons
+//                .stream()
+//                .map(Person::getFullName)
+//                .toList(), List.of("Jason Doe"));
+    }
+
+    @Test
+    void verifyMinDuePerson () throws InterruptedException {
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/tables");
+
+        //Todo: tao 1 list Person lấy text của tất cả các row
+        List<Person> people = driver
+                .findElements(By.xpath("//table[@id='table2']/tbody/tr"))
+                .stream()
+                //Lay text cua tat ca cac row
+                .map(rowText -> rowText.getText())
+                //Tach text ra thanh cac cell, tach bang space
+                .map(rowContent -> rowContent.split(" "))
+                // Tao 1 object Person tu cac cell
+                .map(cellContent -> new Person(cellContent[0], cellContent[1], cellContent[3]))
+                //chuyen ve list
+                .toList();
+
+        //Todo: tim maxDue trong due
+        OptionalDouble minDue = people
+                .stream()
+                .mapToDouble(p -> p.getDue())
+                .min();
+
+        //Todo: tim person co min due
+        List<Person> minDuePersons = people
+                .stream()
+                .filter(p -> p.getDue() == minDue.getAsDouble())
+                .toList();
+
+
+        //Todo: verify
+        Assert.assertEquals(minDuePersons
+                .stream()
+                .map(p -> p.getFullName()).toList(), List.of("Smith John", "Conway Tim"));
+    }
+
+}
--- src/test/java/heroku/WebTableTest.java
+++ src/test/java/heroku/WebTableTest.java
@@ -1,9 +1,101 @@
 package heroku;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class WebTableTest {
     //    Open browser
     //    Navigate to https://the-internet.herokuapp.com/tables
     //    Focus on table 1
     //    The person who has largest due is "Doe Jacson"
+    @Test
+
+    void verifyTable () throws InterruptedException {
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/tables");
+        /**
+         *    1. get row index of max due -> get last name/first name
+         *         due column xpath //table[@id='table1']/tbody/tr/td[4]
+         *         last name column xpath //table[@id='table1']/tbody/tr[row_index]/td[1]
+         *         first name column xpath //table[@id='table1']/tbody/tr[row_index]/td[2]
+         */
+
+        List <Double> dueList = driver.findElements(By.xpath("//table[@id='table1']/tbody/tr/td[4]"))
+                .stream()
+                .map(cell -> Double.valueOf(cell.getText().replace("$","")))
+                .collect(Collectors.toList());
+
+        System.out.println("dueList " + dueList);
+
+        double maxDue = Collections.max(dueList);
+        System.out.println("max " + maxDue);
+        int rowIndex = dueList.indexOf(maxDue) + 1;
+
+        String lastName = driver
+                .findElement(By.xpath("//table[@id='table1']/tbody/tr[" + rowIndex + "]/td[1]"))
+                .getText();
+        String firstName = driver
+                .findElement(By.xpath("//table[@id='table1']/tbody/tr[" + rowIndex + "]/td[2]"))
+                .getText();
+
+        Assert.assertEquals(String.format("%s %s", firstName, lastName), "Jason Doe");
+    }
+
+    @Test
+    void verifyMinDuePerson () throws InterruptedException {
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/tables");
+
+        /**
+         * 1.tao list tat ca cac due
+         * 2. lay list min due
+         * 3. lay index min due
+         * 4. lay last name, first name của row index min due
+         *
+         */
+
+        //todo: tao 1 list  lay tat ca row cua tat ca cac row
+
+        List<String> rowsList = driver
+                .findElements(By.xpath("//table[@id='table2']/tbody/tr"))
+                .stream()
+                .map(WebElement::getText)
+                .toList();
+
+        //todo: tim min due
+        double minDue = rowsList
+                .stream()
+                .map((rowContent) -> rowContent.split(" "))
+                .map(cellContent -> Double.valueOf(cellContent[3].replace("$","")))
+                .min(Double::compareTo)
+                .get();
+
+        System.out.println("minDue = " + minDue);
+
+        List<String> names = rowsList
+                .stream()
+                .map((rowContent) -> rowContent.split(" "))
+                .filter(cellContent -> Double.parseDouble(cellContent[3].replace("$","")) == minDue)
+                .map(cellContent -> String.format("%s %s", cellContent[0], cellContent[1]))
+                .toList();
+
+        System.out.println("names = " + names);
+
+        Assert.assertEquals(names, List.of("Smith John", "Conway Tim"));
+
+
+
+    }
 
 }
