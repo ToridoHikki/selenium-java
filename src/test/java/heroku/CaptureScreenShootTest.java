83c3483c49014374c5a85b79d7254eb09d07d13c
Nhu <truong.thi.huynh.nhu@quantic.com.vn>
2025-05-17 14:07:09 +0700

    change to headless mode

--- src/test/java/heroku/CaptureScreenShootTest.java
+++ src/test/java/heroku/CaptureScreenShootTest.java
@@ -6,17 +6,21 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
+import utils.Browser;
 
 import java.io.File;
 import java.io.IOException;
 
+import static utils.Browser.openBrowser;
+import static utils.Browser.visit;
+
 public class CaptureScreenShootTest {
     @Test
     void captureScreenShot(){
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://the-internet.herokuapp.com/context_menu");
+        openBrowser("chrome");
+        visit("https://the-internet.herokuapp.com/context_menu");
 
-        TakesScreenshot takesScreenshot = (TakesScreenshot) driver;
+        TakesScreenshot takesScreenshot = (TakesScreenshot) Browser.getDriver();
         File srcFile = takesScreenshot.getScreenshotAs(OutputType.FILE);
         File destFile = new File(String.format("target/screenshot-%s-%s.png", "context-menu", System.currentTimeMillis()));
         try {
