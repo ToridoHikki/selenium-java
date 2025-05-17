fb4c1994aa3833f82ae8b05166903b75db092c09
Nhu <truong.thi.huynh.nhu@quantic.com.vn>
2025-05-17 14:39:34 +0700

    edit: change to headless mode in NestedFrameTest

--- src/test/java/heroku/NestedFrameTest.java
+++ src/test/java/heroku/NestedFrameTest.java
@@ -5,30 +5,34 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import utils.Browser;
+
+import static utils.Browser.openBrowser;
+import static utils.Browser.visit;
 
 public class NestedFrameTest {
 
     @Test
     void verifyNestedFrame () throws InterruptedException {
 
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://the-internet.herokuapp.com/nested_frames");
+        openBrowser("chrome");
+        visit("https://the-internet.herokuapp.com/nested_frames");
 
-        driver.switchTo().frame(0);
-        driver.switchTo().frame(0);
-        Assert.assertTrue(driver.findElement(By.tagName("body")).getText().contains("LEFT"));
+        Browser.getDriver().switchTo().frame(0);
+        Browser.getDriver().switchTo().frame(0);
+        Assert.assertTrue(Browser.getDriver().findElement(By.tagName("body")).getText().contains("LEFT"));
 
-        driver.switchTo().parentFrame();
-        driver.switchTo().frame(1);
-        Assert.assertTrue(driver.findElement(By.tagName("body")).getText().contains("MIDDLE"));
+        Browser.getDriver().switchTo().parentFrame();
+        Browser.getDriver().switchTo().frame(1);
+        Assert.assertTrue(Browser.getDriver().findElement(By.tagName("body")).getText().contains("MIDDLE"));
 
-        driver.switchTo().parentFrame();
-        driver.switchTo().frame("frame-right");
-        Assert.assertTrue(driver.findElement(By.tagName("body")).getText().contains("RIGHT"));
+        Browser.getDriver().switchTo().parentFrame();
+        Browser.getDriver().switchTo().frame("frame-right");
+        Assert.assertTrue(Browser.getDriver().findElement(By.tagName("body")).getText().contains("RIGHT"));
 
-        driver.switchTo().parentFrame();
-        driver.switchTo().frame("frame-bottom");
-        Assert.assertTrue(driver.findElement(By.tagName("body")).getText().contains("BOTTOM"));
+        Browser.getDriver().switchTo().parentFrame();
+        Browser.getDriver().switchTo().frame("frame-bottom");
+        Assert.assertTrue(Browser.getDriver().findElement(By.tagName("body")).getText().contains("BOTTOM"));
 
     }
 }
