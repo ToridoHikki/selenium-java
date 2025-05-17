54fd1cce3eb18a5ab258b0a4d792f92e37482b43
Nhu <truong.thi.huynh.nhu@quantic.com.vn>
2025-05-17 14:15:01 +0700

    change to headless mode

--- src/test/java/heroku/DropdownTest.java
+++ src/test/java/heroku/DropdownTest.java
@@ -7,11 +7,23 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import utils.Browser;
 
 import java.time.Duration;
 
+import static utils.Browser.openBrowser;
+import static utils.Browser.visit;
+
 public class DropdownTest {
+
+
+    @BeforeMethod
+    void setup() {
+        openBrowser("chrome");
+    }
 
     @Test
     void tc03 () {
@@ -21,76 +33,61 @@ public class DropdownTest {
 //        Select "option 1"
 //        Validate "option 1" is selected
 
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://the-internet.herokuapp.com/dropdown");
+        visit("https://the-internet.herokuapp.com/dropdown");
+        Browser.getDriver().findElement(By.id("dropdown")).click();
+        Browser.getDriver().findElement(By.cssSelector("option[value='1']")).click();
 
-        driver.findElement(By.id("dropdown")).click();
-        driver.findElement(By.cssSelector("option[value='1']")).click();
-//        Assert.assertTrue(driver.findElement((By.cssSelector("option[selected=selected]"))).isDisplayed());
+        Assert.assertTrue(Browser.getDriver().findElement(By.cssSelector("option[selected=selected]")).isDisplayed());
 
-        Assert.assertTrue(driver.findElement(By.cssSelector("option[selected=selected]")).isDisplayed());
-
-        driver.quit();
+        Browser.getDriver().quit();
 
     }
 
     @Test
     void verifyDropdownWorking() throws InterruptedException {
+        visit("https://the-internet.herokuapp.com/dropdown");
 
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://the-internet.herokuapp.com/dropdown");
-
-        Select select = new Select(driver.findElement(By.id("dropdown")));
+        Select select = new Select(Browser.getDriver().findElement(By.id("dropdown")));
 
         select.selectByContainsVisibleText("Option 1");
-        Assert.assertTrue(driver.findElement(By.xpath("//optiion[.='Option 1']")).isDisplayed());
+        Assert.assertTrue(Browser.getDriver().findElement(By.xpath("//optiion[.='Option 1']")).isDisplayed());
 
         select.selectByValue("1");
-        Assert.assertTrue(driver.findElement(By.cssSelector("option[value='1']")).isDisplayed());
+        Assert.assertTrue(Browser.getDriver().findElement(By.cssSelector("option[value='1']")).isDisplayed());
 
         select.selectByIndex(1);
-        Assert.assertTrue(driver.findElement(By.xpath("//select/option[1]")).isDisplayed());
+        Assert.assertTrue(Browser.getDriver().findElement(By.xpath("//select/option[1]")).isDisplayed());
     }
 
     @Test
     void ableToSelectMultipleOption () throws InterruptedException {
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://output.jsbin.com/osebed/2");
+        Browser.getDriver().get("https://output.jsbin.com/osebed/2");
     }
 
     @Test
     void verifyTextfieldDisabled () throws InterruptedException {
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://the-internet.herokuapp.com/dynamic_controls");
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        Browser.getDriver().get("https://the-internet.herokuapp.com/dynamic_controls");
+        WebDriverWait wait = new WebDriverWait(Browser.getDriver(), Duration.ofSeconds(10));
 
-        Assert.assertFalse(driver.findElement(By.cssSelector("form#input-example input")).isEnabled());
+        Assert.assertFalse(Browser.getDriver().findElement(By.cssSelector("form#input-example input")).isEnabled());
 
-        driver.findElement(By.cssSelector("from#input-example button")).click();
+        Browser.getDriver().findElement(By.cssSelector("from#input-example button")).click();
 
         Assert.assertTrue(wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("form#input-example input"))).isEnabled());
-
-
-        driver.quit();
     }
 
     @Test
     void  verifyLoginSuccessWithValidCredentials() throws InterruptedException {
-        WebDriver driver = new ChromeDriver();
+        visit("https://admin:admin@the-internet.herokuapp.com/basic_auth");
 
-        driver.get("https://admin:admin@the-internet.herokuapp.com/basic_auth");
-
-        String welcomeText = driver.findElement(By.cssSelector("div.example h3")).getText();
+        String welcomeText = Browser.getDriver().findElement(By.cssSelector("div.example h3")).getText();
 
         System.out.println(welcomeText);
+    }
 
-        //prinrt welcome text
-
-
-
-
-
-
+    @AfterMethod
+    void teardown() {
+        Browser.quit();
     }
 
 
